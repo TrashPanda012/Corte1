cc9a8d49ecf8b3f669487a087be516f761b5b3b2
TrashPanda012 <kalbrowne@uamv.edu.ni>
2024-09-23 08:48:23 -0600

    profe hasta ahorita leo que había que hacer 5 commits pq estaba al final de las indicaciones :C

--- Corte1/Corte1/Form1.Designer.cs
+++ Corte1/Corte1/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNombres = new System.Windows.Forms.TextBox();
+            this.tbApellidos = new System.Windows.Forms.TextBox();
+            this.dtpBirthday = new System.Windows.Forms.DateTimePicker();
+            this.cbCiudades = new System.Windows.Forms.ComboBox();
+            this.btnAgregarPersona = new System.Windows.Forms.Button();
+            this.btnCalcularEdad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fecha de nacimiento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ciudad:";
+            // 
+            // tbNombres
+            // 
+            this.tbNombres.Location = new System.Drawing.Point(167, 99);
+            this.tbNombres.Name = "tbNombres";
+            this.tbNombres.Size = new System.Drawing.Size(339, 22);
+            this.tbNombres.TabIndex = 4;
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.Location = new System.Drawing.Point(167, 127);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(339, 22);
+            this.tbApellidos.TabIndex = 5;
+            // 
+            // dtpBirthday
+            // 
+            this.dtpBirthday.Location = new System.Drawing.Point(167, 155);
+            this.dtpBirthday.Name = "dtpBirthday";
+            this.dtpBirthday.Size = new System.Drawing.Size(339, 22);
+            this.dtpBirthday.TabIndex = 6;
+            // 
+            // cbCiudades
+            // 
+            this.cbCiudades.FormattingEnabled = true;
+            this.cbCiudades.Location = new System.Drawing.Point(167, 183);
+            this.cbCiudades.Name = "cbCiudades";
+            this.cbCiudades.Size = new System.Drawing.Size(339, 24);
+            this.cbCiudades.TabIndex = 7;
+            // 
+            // btnAgregarPersona
+            // 
+            this.btnAgregarPersona.Location = new System.Drawing.Point(126, 288);
+            this.btnAgregarPersona.Name = "btnAgregarPersona";
+            this.btnAgregarPersona.Size = new System.Drawing.Size(131, 45);
+            this.btnAgregarPersona.TabIndex = 8;
+            this.btnAgregarPersona.Text = "Agregar Persona";
+            this.btnAgregarPersona.UseVisualStyleBackColor = true;
+            this.btnAgregarPersona.Click += new System.EventHandler(this.btnAgregarPersona_Click);
+            // 
+            // btnCalcularEdad
+            // 
+            this.btnCalcularEdad.Location = new System.Drawing.Point(268, 288);
+            this.btnCalcularEdad.Name = "btnCalcularEdad";
+            this.btnCalcularEdad.Size = new System.Drawing.Size(121, 45);
+            this.btnCalcularEdad.TabIndex = 9;
+            this.btnCalcularEdad.Text = "Calcular Edad";
+            this.btnCalcularEdad.UseVisualStyleBackColor = true;
+            this.btnCalcularEdad.Click += new System.EventHandler(this.btnCalcularEdad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(518, 450);
+            this.Controls.Add(this.btnCalcularEdad);
+            this.Controls.Add(this.btnAgregarPersona);
+            this.Controls.Add(this.cbCiudades);
+            this.Controls.Add(this.dtpBirthday);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.tbNombres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbNombres;
+        private System.Windows.Forms.TextBox tbApellidos;
+        private System.Windows.Forms.DateTimePicker dtpBirthday;
+        private System.Windows.Forms.ComboBox cbCiudades;
+        private System.Windows.Forms.Button btnAgregarPersona;
+        private System.Windows.Forms.Button btnCalcularEdad;
     }
 }
 
--- Corte1/Corte1/Form1.cs
+++ Corte1/Corte1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Corte1.models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,74 @@ namespace Corte1
 {
     public partial class Form1 : Form
     {
+        private Registro registro;
+        private Operacion operacion;
         public Form1()
         {
             InitializeComponent();
+            registro = new Registro();
+            operacion = new Operacion();
+            CargarCiudades();
+        }
+
+        private void CargarCiudades()
+        {
+            cbCiudades.Items.Add("Managua");
+            cbCiudades.Items.Add("Masaya");
+            cbCiudades.Items.Add("Granada");
+            cbCiudades.Items.Add("Boaco");
+            cbCiudades.Items.Add("Carazo");
+            cbCiudades.Items.Add("Chinandega");
+            cbCiudades.Items.Add("Chontales");
+            cbCiudades.Items.Add("Estelí");
+            cbCiudades.Items.Add("Jinotega");
+            cbCiudades.Items.Add("León");
+            cbCiudades.Items.Add("Madriz");
+            cbCiudades.Items.Add("Matagalpa");
+            cbCiudades.Items.Add("Nueva Segovia");
+            cbCiudades.Items.Add("Rivas");
+            cbCiudades.Items.Add("Rio San Juan");
+        }
+        private void LimpiarFormulario()
+        {
+            tbNombres.Clear();
+            tbApellidos.Clear();
+            cbCiudades.SelectedIndex = -1;
+            dtpBirthday.Value = DateTime.Now;
+        }
+
+        private void btnAgregarPersona_Click(object sender, EventArgs e)
+        {
+            string nombres = tbNombres.Text;
+            string apellidos = tbApellidos.Text;
+            DateTime fechaNacimiento = dtpBirthday.Value;
+            string ciudad = cbCiudades.SelectedItem?.ToString() ?? "";
+
+            if (string.IsNullOrWhiteSpace(nombres) || string.IsNullOrWhiteSpace(apellidos) || string.IsNullOrWhiteSpace(ciudad))
+            {
+                MessageBox.Show("Los campos no deben quedar en blanco");
+                return;
+            }
+
+            Persona nuevaPersona = new Persona(nombres, apellidos, fechaNacimiento, ciudad);
+            registro.AgregarPersona(nuevaPersona);
+            MessageBox.Show("Persona agregada correctamente.");
+            LimpiarFormulario();
+        }
+
+        private void btnCalcularEdad_Click(object sender, EventArgs e)
+        {
+            if (registro.Contador > 0)
+            {
+                Persona ultimaPersona = registro.ObtenerPersona(registro.Contador - 1);
+                int edad = operacion.CalcularEdad(ultimaPersona);
+                string mensaje = $"Edad: {edad} años, {operacion.EsMayorDeEdad(ultimaPersona)}";
+                MessageBox.Show(mensaje);
+            }
+            else
+            {
+                MessageBox.Show("Debe agregar a alguien para calcular la edad.");
+            }
         }
     }
 }
